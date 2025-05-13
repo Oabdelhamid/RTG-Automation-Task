36647d1a48ade59a1814473e60042c17bd5ae955
Oabdelhamid <100126534+Oabdelhamid@users.noreply.github.com>
2025-05-13 20:28:44 +0300

    CI/CD With allure reporting

--- src/test/java/Tests/TestCase.java
+++ src/test/java/Tests/TestCase.java
@@ -12,6 +12,8 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
+import java.time.Duration;
+
 public class TestCase {
     protected WebDriver driver;
 
@@ -19,6 +21,9 @@ public class TestCase {
     @BeforeClass
     public void setup( String browser) {
    driver = DriverFactory.createDriver(browser);
+   driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(90));
+   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+   driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(60));
 
     }
 
